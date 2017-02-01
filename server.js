1048cf793f4f6d5f02407ab8493e82487f76566b
Nii Equity <nii.nmarley@gmail.com>
2017-02-01 22:12:31 +0000

    No authorization middleware, create account, authenticate user and minor fixes

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var jwt = require('jsonwebtoken');
 var Book = require('./models/book');
 var User = require('./models/user');
 
@@ -20,32 +21,89 @@ app.use(bodyParser.json());
 var router = express.Router();
 
 router.use(function(req, res, next) {
-    // Check here for authorization
-    next(); // make sure we go to the next routes and don't stop here
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, 'specialforces', function(err, decoded) {      
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;    
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({ 
+        message: 'unauthorized access. Create account at /users and authenticate at /authenticate' 
+    });
+    
+  }
 });
 
 router.get('/', function(req, res) {
 	res.json({'message': 'first'});
 })
 
-router.route('/users')
-	
-	.post(function(req, res){
-		var user = new User();
-		user.username = req.body.username;
-		user.password = req.body.password;
+app.post('/users', function(req, res){
+	var user = new User();
+	user.username = req.body.username;
+	user.password = req.body.password;
 
-		user.save(function(err){
-			if(err)
-				res.send(err)
-			res.json({message: 'User created Successfully.'})
-		})
+	user.save(function(err){
+		if(err)
+			res.send(err)
+		res.json({message: 'User created Successfully.'})
 	})
+});
+
+app.post('/authenticate', function(req, res){
+   	
+   	User.findOne({
+	    username: req.body.username
+	  }, function(err, user){
+
+	    if (err) throw err;
+
+	    if (!user){
+	      res.json({message: 'User not found.'});
+	    } else if (user){
+
+	      // check if password matches
+	      if (user.password != req.body.password) {
+	        res.json({message: 'Wrong password.'});
+	      } else {
+
+	        // if user is found and password is right
+	        // create a token
+	        var token = jwt.sign(user, 'specialforces', {
+	          expiresIn: 60*24 // expires in 24 hours
+	        });
+
+	        // return the information including token as JSON
+	        res.json({
+	          message: 'Token Generated. Append to all requests',
+	          token: token
+	        });
+	      }   
+
+	    }
+
+	  });
+});
 
 
 router.route('/books')
 
-	.get(function(req, res) {
+	.get(function(req, res){
 		Book.find(function(err, books) {
 			if(err)
 				res.send(err)
@@ -53,7 +111,7 @@ router.route('/books')
 		})
 	})
 	
-	.post(function(req, res) {
+	.post(function(req, res){
         
     var book = new Book();     
     book.title = req.body.title; 
@@ -70,18 +128,18 @@ router.route('/books')
 
 router.route('/books/:btitle')
 
-  .get(function(req, res) {
-      Book.find({title: req.params.btitle}, function(err, book) {
+  .get(function(req, res){
+      Book.find({title: req.params.btitle}, function(err, book){
           if (err)
               res.send(err);
           res.json(book);
       });
   })
 
-  .delete(function(req, res) {
+  .delete(function(req, res){
       Book.remove({
           title: req.params.btitle
-      }, function(err, book) {
+      }, function(err, book){
           if (err)
               res.send(err);
 
@@ -89,7 +147,7 @@ router.route('/books/:btitle')
       });
   });
 
-app.use('/', router);
+app.use('/api', router);
 app.listen(8081)
 
 console.log("Server started on port 8081")
